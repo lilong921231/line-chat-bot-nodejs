fee43d0b5bf28c6c9be6fbb5b810bc7cfa685289
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-21 13:07:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const config = require('./CHANNEL_CONFIG');
-const initial = require('./init-bot')
+const lineBot = require('linebot');
+const express = require('express');
+const reply = require('./msg-reply');
 
-initialize();
+const bot = lineBot({
+    channelId: "1648099378",
+    channelSecret: "2fe8f7a52d94b031b7d002688bda80db",
+    channelAccessToken: "wjJikM9SGt8DmurAb14g20haoST6tMLq2Q9qJnUUZfkr/E7gKEYdAmT9rDj1efz/c6Zf8ISCNrzm+6SkQUZxNPZB4obXI60HlC8b8bVDqT1nBhpCylwfk8OpnjPf+wtbxCkVmCda8wcQBPM3BkdyIwdB04t89/1O/w1cDnyilFU="
+});
 
-function initialize() {
-    initial.initBot(config);
-}
+bot.on('message', function (event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+    console.log(event.message); //把收到訊息的 event 印出來看看
+    console.log(reply.msgReply);
+    event.reply(event.message.text).then(function (data) {
+        // success
+        console.log("=============success==============");
+        console.log(event.message.text);
+        console.log(data);
+    }).catch(function (error) {
+        // error
+        console.log("=============error==============");
+        console.log(error);
+    });
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//express port:3000
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port:", port);
+});
 
