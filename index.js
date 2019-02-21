77ccfd15b36ff2f0e5be24c7c14eac80de9bc1b8
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-21 11:15:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const express = require('express');
 const reply = require('./msgReply');
 
 const bot = linebot({
-  channelId: "1648099378",
-  channelSecret: "4b18885a067aaa5c27123e6ec66f3a42",
+  channelId: "1648295438",
+  channelSecret: "2fe8f7a52d94b031b7d002688bda80db",
   channelAccessToken: "wjJikM9SGt8DmurAb14g20haoST6tMLq2Q9qJnUUZfkr/E7gKEYdAmT9rDj1efz/c6Zf8ISCNrzm+6SkQUZxNPZB4obXI60HlC8b8bVDqT1nBhpCylwfk8OpnjPf+wtbxCkVmCda8wcQBPM3BkdyIwdB04t89/1O/w1cDnyilFU="
 });
 
 bot.on('message', function(event) {
   console.log(event); //把收到訊息的 event 印出來看看
   console.log(event.message); //把收到訊息的 event 印出來看看
-  event.reply(reply.msgReply(event.message.text)).then(function (data) {
+  event.reply(event.message.text).then(function (data) {
     // success
 	console.log("=============success==============");
 	console.log(event.message.text);
@@ -39,5 +39,5 @@ var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port:", port);
   console.log(config);
-  console.log(reply);
+  console.log(reply.msgReply(1234));
 });
