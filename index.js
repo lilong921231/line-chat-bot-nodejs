0f240778995d88084b56eaf6ab01259f5b4ee785
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-21 09:41:16 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+require('./ChannelConfig');
 var linebot = require('linebot');
 var express = require('express');
+var reply = require('./msgReply');
 
 var bot = linebot({
-  channelId: "1648295438",
-  channelSecret: "951b7c6d4778bdb1b101b4b33d207318",
-  channelAccessToken: "UkikIwFZvMOErpl3tFXesFu/fAQ54V/ubCNPzdpTbQxWxxA7ipjAbbW3JxmHf47cc6Zf8ISCNrzm+6SkQUZxNPZB4obXI60HlC8b8bVDqT3p6b6AQJxOB6fDGwHnZybX4qO0dydDCaq2bIRvkIzkYQdB04t89/1O/w1cDnyilFU="
+  channelId: CHANNEL_ID,
+  channelSecret: "4b18885a067aaa5c27123e6ec66f3a42",
+  channelAccessToken: "klUTk/W5MuwoP1OG4njZM1zlaOdjW0CLYrmXCQiosr3+IVj2jxvxGanLVv/7lhxvLHSKNAChYYQjUp3kLhXeFuSTmgrdR7jq1ORWIHFKOK8rbAx7rG0lNCAluxle0UojeSdZwrbRi4H9jXEXL0BkVwdB04t89/1O/w1cDnyilFU="
 });
 
 bot.on('message', function(event) {
-  console.log("=============event==============");
   console.log(event); //把收到訊息的 event 印出來看看
   console.log(event.message); //把收到訊息的 event 印出來看看
-  event.reply(event.message.text).then(function (data) {
+  event.reply(reply.msgReply(event.message.text)).then(function (data) {
     // success
-// 	console.log("=============success==============");
-// 	console.log(event.message.text);
-// 	console.log(data);
+	console.log("=============success==============");
+	console.log(event.message.text);
+	console.log(data);
   }).catch(function (error) {
     // error
-// 	console.log("=============error==============");
-// 	console.log(error);
+	console.log("=============error==============");
+	console.log(error);
   });
 });
 
@@ -29,8 +30,6 @@ app.post('/', linebotParser);
 
 //express port:3000
 var server = app.listen(process.env.PORT || 8080, function() {
-	console.log("=============process==============");
-	console.log(process.env.PORT);
   var port = server.address().port;
   console.log("App now running on port:", port);
 });
