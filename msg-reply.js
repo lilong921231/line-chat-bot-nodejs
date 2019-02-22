a6e77c7788f20d8e6f8389dab695a58df12656f8
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-22 09:16:18 +0900

    Update msg-reply.js

--- msg-reply.js
+++ msg-reply.js
@@ -1,40 +1,7 @@
 function msgReply(text) {
-    /*var talk_block = {
-        "type": "flex",
-        "altText": "Flex Message",
-        "contents": {
-            "type": "bubble",
-            "direction": "ltr",
-            "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                    {
-                        "type": "text",
-                        "text": "おはようございます。今日は月曜日ですね。今日からまた新しい一週間の始まりです。\n現在の口座残高は657,980円です。\n今週の口座入出金予定をご案内します。",
-                        "weight": "regular",
-                        "wrap": true
-                    }
-                ]
-            },
-            "footer": {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [
-                    {
-                        "type": "button",
-                        "action": {
-                            "type": "message",
-                            "label": "OK",
-                            "text": "OK"
-                        },
-                        "color": "#5B9BD5"
-                    }
-                ]
-            }
-        }
-    }:*/
-
+    if (text !== '1') {
+        return text;
+    }
     var talk_block = [
         {
             "type": "text",
@@ -74,6 +41,7 @@ function msgReply(text) {
             }
         }
     ];
+    
     return talk_block;
 }
 
