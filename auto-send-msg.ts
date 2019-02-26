6a87c6ac5bccb78aaa940538e2a99cc99d7806dc
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-26 16:35:17 +0900

    Update auto-send-msg.ts

--- auto-send-msg.ts
+++ auto-send-msg.ts
@@ -1,45 +1,45 @@
-const schedule = require('node-schedule');
+// const schedule = require('node-schedule');
 
-function scheduleExec() {
-    console.log('scheduleExec:' + new Date());
-    scheduleObjectSendMessage();
-    scheduleRecurrenceRule();
-}
+// function scheduleExec() {
+//     console.log('scheduleExec:' + new Date());
+//     scheduleObjectSendMessage();
+//     scheduleRecurrenceRule();
+// }
 
 
-function scheduleObjectSendMessage(){
+// function scheduleObjectSendMessage(){
 
-    const rule = new schedule.RecurrenceRule();
-    const userIds = require("line-user-friends");
+//     const rule = new schedule.RecurrenceRule();
+//     const userIds = require("line-user-friends");
 
-    //second minute hour dayOfMonth month dayOfWeek
-    rule.minute = 35;
-    rule.hour = 11;
-    rule.dayOfWeek =[1,2,3,4,5,6,7];
-    schedule.scheduleJob(rule, function(){
-        console.log('现在时间：',new Date());
-        for (let i = 0; i < userIds.length; i++) {
-            console.log(userIds[i]);
-        }
-    });
-}
+//     //second minute hour dayOfMonth month dayOfWeek
+//     rule.minute = 35;
+//     rule.hour = 11;
+//     rule.dayOfWeek =[1,2,3,4,5,6,7];
+//     schedule.scheduleJob(rule, function(){
+//         console.log('现在时间：',new Date());
+//         for (let i = 0; i < userIds.length; i++) {
+//             console.log(userIds[i]);
+//         }
+//     });
+// }
 
-function scheduleRecurrenceRule(){
+// function scheduleRecurrenceRule(){
 
-    var rule = new schedule.RecurrenceRule();
-    // rule.dayOfWeek = 2;
-    // rule.month = 3;
-    // rule.dayOfMonth = 1;
-    // rule.hour = 1;
-    // rule.minute = 42;
-    rule.second = 0;
+//     var rule = new schedule.RecurrenceRule();
+//     // rule.dayOfWeek = 2;
+//     // rule.month = 3;
+//     // rule.dayOfMonth = 1;
+//     // rule.hour = 1;
+//     // rule.minute = 42;
+//     rule.second = 0;
 
-    schedule.scheduleJob(rule, function(){
-        console.log('scheduleRecurrenceRule:' + new Date());
-    });
+//     schedule.scheduleJob(rule, function(){
+//         console.log('scheduleRecurrenceRule:' + new Date());
+//     });
 
-}
+// }
 
-module.exports = {
-    scheduleExec
-};
+// module.exports = {
+//     scheduleExec
+// };
